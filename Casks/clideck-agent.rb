5488af3b8216de8f8e48ba60202ead5d51d0026b
SmartRoot7 <miloserdov.v@gmail.com>
2025-08-24 21:26:32 -0500

    chore(tap): bump to v0.1.5

--- Casks/clideck-agent.rb
+++ Casks/clideck-agent.rb
@@ -1,5 +1,5 @@
 cask "clideck-agent" do
-  version "0.1.4"
+  version "0.1.5"
   sha256 :no_check
 
   url "https://github.com/SmartRoot7/CliDeck/releases/download/v#{version}/CliDeckAgent-#{version}.zip"
